54c619d772b81c318acf9da3f5970a685bc4d4e3
warugaki webDeveloper <warugaki.web.developer@gmail.com>
2021-03-13 17:58:33 +0900

    開発サーバーの設定

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: {
     main: path.resolve(__dirname, './src/index.js')
   },
@@ -13,9 +14,19 @@ module.exports = {
     filename: '[name].js'
   },
 
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, './dist'),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080
+  },
+
   plugins: [
     new webpack.ProgressPlugin(),
     new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       title: 'webpack base',
       template: path.resolve(__dirname, './src/template.html'),
