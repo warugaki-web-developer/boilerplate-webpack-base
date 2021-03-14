c031b28bb61ca5ea371ebc9a1611a4079bc5352c
warugaki webDeveloper <warugaki.web.developer@gmail.com>
2021-03-14 11:42:02 +0900

    babel polyfillの設定とテストファイルの追加

---
+++ src/getClasses.js
@@ -0,0 +1,13 @@
+import { wizard } from './wizard'
+import { barbarian } from './barbarian'
+
+function getClasses() {
+  // eslint-disable-next-line no-console
+  console.log('get classes was called')
+  // eslint-disable-next-line no-console
+  console.log(wizard)
+  // eslint-disable-next-line no-console
+  console.log(barbarian)
+}
+
+export default getClasses
--- src/index.js
+++ src/index.js
@@ -1,5 +1,70 @@
+/**
+ * These commented out imports are the alternative to:
+ * "useBuiltIns": "usage"
+ * in the .babelrc
+ */
+// import "core-js/modules/es.object.values";
+// import "core-js/modules/es.promise";
+
+/**
+ * This is required for generators, including async/await
+ * Note: It seems that async/await is not built on generators,
+ * but babel transpiles is to one for backwards support.
+ */
+// import "regenerator-runtime/runtime";
+
 import './index.scss'
 
-const elm = document.getElementById('root')
+import getClasses from './getClasses'
 
-elm.textContent = 'hello webpack'
+getClasses()
+
+const elvenShieldRecipe = {
+  leatherStrips: 2,
+  ironIngot: 1,
+  refinedMoonstone: 4
+}
+
+// ES7 Object spread example
+const elvenGauntletsRecipe = {
+  ...elvenShieldRecipe,
+  leather: 1,
+  refinedMoonstone: 1
+}
+// eslint-disable-next-line no-console
+console.log('ES7 Object spread example: ', elvenGauntletsRecipe)
+
+// ES8 Object.values example
+// Note: Will not transpile without babel/imported polyfills because it is a new method
+// eslint-disable-next-line no-console
+console.log('ES8 Object.values example', Object.values(elvenGauntletsRecipe))
+
+// Event queue block scoping example
+// Check babel output to see that `let` isn't simply switched to `var`
+// because the code would not have the same output.
+for (let i = 0; i < 10; i++) {
+  setTimeout(function () {
+    // eslint-disable-next-line no-console
+    console.log(i)
+  }, 1)
+}
+
+// async/await example from MDN
+function resolveAfter2Seconds() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('resolved')
+    }, 2000)
+  })
+}
+
+async function asyncCall() {
+  // eslint-disable-next-line no-console
+  console.log('calling')
+  const result = await resolveAfter2Seconds()
+  // eslint-disable-next-line no-console
+  console.log(result)
+  // expected output: "resolved"
+}
+
+asyncCall()
